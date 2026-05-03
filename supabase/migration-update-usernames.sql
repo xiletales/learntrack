d9bbf89618187954bc89f2166ec2a1621c28f7b8
xiletales <naurayumna06@gmail.com>
2026-05-03 22:30:47 +0700

    fix: add migration for profile usernames

---
+++ supabase/migration-update-usernames.sql
@@ -0,0 +1,7 @@
+-- Update usernames for all profiles
+UPDATE public.profiles SET username = 'budi.santoso' WHERE id = '9c3ab768-a3ae-40e2-bfac-8b2d897c1ef1';
+UPDATE public.profiles SET username = 'ani.widyastuti' WHERE id = 'd0de0d68-986d-4874-8f57-cabbde7e3772';
+UPDATE public.profiles SET username = 'naurayumna06' WHERE id = 'a7b3f125-9f78-42db-b996-5350043a134d';
+UPDATE public.profiles SET username = 'zahhid' WHERE id = '8307d745-6e36-4e74-8914-37dd8ea2cdb3';
+UPDATE public.profiles SET username = 'naurayumna24' WHERE id = '1c988114-6189-498c-a059-68d265001093';
+UPDATE public.profiles SET username = 'syahruzzahid' WHERE id = '001204f5-627e-43ee-884f-c7f56f5e15f8';
